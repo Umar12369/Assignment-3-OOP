69302989bfde3697069ad1d4f28ab4628fd6297b
Umar12369 <Omar_aizan@hotmail.com>
2022-12-15 11:09:49 -0500

    select account methond created

--- Program.py
+++ Program.py
@@ -46,12 +46,42 @@ class Program:
             else:
                 print("Invalid choice. Please try again.")
 
-    
-    
-    
-    
-    
-    
+    def open_account(self):
+        # Prompt the user for account details and create an account object
+        account_number = input("Enter the account number: ")
+        account_holder_name = input("Enter the account holder's name: ")
+        rate_of_interest = input("Enter the rate of interest: ")
+        balance = input("Enter the balance: ")
+        account_type = input("Enter the account type (1 for Savings, 2 for Checking): ")
+        
+        if account_type == "1":
+        # Create a SavingsAccount object
+            min_balance = input("Enter the minimum balance: ")
+            account = SavingsAccount(account_number, account_holder_name, rate_of_interest, balance, min_balance)
+        elif account_type == "2":
+        # Create a ChecquingAccount object
+            overdraft_limit = input("Enter the overdraft limit: ")
+            account = ChecquingAccount(account_number, account_holder_name, rate_of_interest, balance, overdraft_limit)
+        else:
+            print("Invalid account type. Please try again.")
+            return
+        
+        # Add the account to the accounts dictionary
+        self.accounts[account_number] = account
+        print("Account created successfully.")
+
+
+    def select_account(self):
+        # Prompt the user for the account number
+        account_number = input("Enter the account number: ")
+        
+        # Search for the account in the accounts dictionary
+        if account_number in self.accounts:
+            account = self.accounts[account_number]
+        # If found, call show_account_menu() with the account object as argument
+            self.show_account_menu(account)
+        else:
+            print("Account not found. Please try again.")
     
     def run(self):
         self.show_main_menu()
