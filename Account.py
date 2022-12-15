4a8a8ecbad63a2f82e15672e625552c06ddd093b
Umar12369 <Omar_aizan@hotmail.com>
2022-12-15 10:44:16 -0500

    class acount with self variables

---
+++ Account.py
@@ -0,0 +1,7 @@
+
+class Account:
+  def __init__(self, account_number, account_holder_name, rate_of_interest, balance):
+    self.account_number = account_number
+    self.account_holder_name = account_holder_name
+    self.rate_of_interest = rate_of_interest
+    self.balance = balance
