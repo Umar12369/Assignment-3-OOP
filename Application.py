932a9171ab96ebf85c95b44dc55fcf7f1d9e4d4f
Umar12369 <Omar_aizan@hotmail.com>
2022-12-15 10:17:39 -0500

    the ShowMainMenu Method and While Loop

--- Application.py
+++ Application.py
@@ -1,4 +1,20 @@
 class Program:
   def __init__(self):
-    self.accounts = {} #dictionary to store values 
-    
+    self.accounts = {} #dictionary to store values
+  
+  def show_main_menu(self):
+    while True:
+      print("Banking Main Menu:")
+      print("1. Open Account")
+      print("2. Select Account")
+      print("3. Exit")
+      choice = input("Enter your choice: ")
+      
+      if choice == "1":
+        self.open_account()
+      elif choice == "2":
+        self.select_account()
+      elif choice == "3":
+        break
+      else:
+        print("Invalid choice. Please try again.")
